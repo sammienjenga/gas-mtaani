3b8ef2a14c6d2783e53cae5af946868b69c2543f
sammienjenga <njengakarau74@gmail.com>
2026-05-22 16:19:23 +0300

    fix: add render production domain to ALLOWED_HOSTS

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -12,7 +12,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.getenv('SECRET_KEY') 
 DEBUG = True
-ALLOWED_HOSTS = ['localhost','127.0.0.1']
+ALLOWED_HOSTS = ['localhost','127.0.0.1','gas-mtaani.onrender.com']
 
 INSTALLED_APPS = [ 
     'corsheaders',
